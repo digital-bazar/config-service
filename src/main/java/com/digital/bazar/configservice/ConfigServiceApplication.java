35b18389c45d678cda78a4cbd53b5227b83fda2f
Murali Boyapati <murali.boyapati@epsilon.com>
2019-01-21 13:59:16 -0600

    Shell

---
+++ src/main/java/com/digital/bazar/configservice/ConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.digital.bazar.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
+
